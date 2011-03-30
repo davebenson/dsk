c6d3235e52edd9ba06949032a53cf877b09fa224
Dave Benson <daveb@ffem.org>
2011-03-29 17:10:28 -0700

    experiment with mini-language

---
+++ dsk-zlang.c
@@ -0,0 +1,95 @@
+
+typedef struct _DskZLangTypeInfo DskZLangTypeInfo;
+
+typedef enum
+{
+  DSK_ZLANG_NAMESPACE_ENTRY_TYPE,
+  DSK_ZLANG_NAMESPACE_ENTRY_GLOBAL,
+  DSK_ZLANG_NAMESPACE_ENTRY_FUNCTION
+} DskZLangNamespaceEntryType;
+
+typedef struct _DskZLangNamespaceEntry DskZLangNamespaceEntry;
+struct _DskZLangNamespaceEntry
+{
+  DskZLangNamespaceEntryType type;
+  char *name;
+  union {
+    struct {
+      void (*clear)(DskZLangTypeInfo        *type_info,
+                    DskZLangStackNode       *value);
+      void (*copy) (DskZLangTypeInfo        *type_info,
+                    DskZLangStackNode       *dst,
+                    const DskZLangStackNode *src);
+    } type;
+    struct {
+      DskZLangStackNode value;
+    } global;
+    struct {
+      DskZLangFunction *variadic;
+      unsigned n_arities;
+      DskZLangFunction **arities;
+    } function;
+    DskZLangNamespace *ns;
+  } u;
+
+  /* tree structure parts */
+  DskZLangNamespaceEntry *left, *right, *parent;
+  dsk_boolean is_red;
+};
+
+struct _DskZLangNamespace
+{
+  DskZLangNamespaceEntry *tree;
+};
+
+struct _DskZLangContext
+{
+  DskZLangNamespace namespace;
+};
+
+
+DskZLangContext *dsk_zlang_context_new        (void)
+{
+  DskZLangContext *rv = dsk_malloc (sizeof (DskZLangContext));
+  rv->namespace.tree = NULL;
+  dsk_zlang_namespace_register_type (&rv->namespace, "int",
+                                     dsk_zlang_stack_node_clear_do_nothing,
+                                     dsk_zlang_stack_node_copy_memcpy);
+  dsk_zlang_namespace_register_type (&rv->namespace, "uint",
+                                     dsk_zlang_stack_node_clear_do_nothing,
+                                     dsk_zlang_stack_node_copy_memcpy);
+  dsk_zlang_namespace_register_type (&rv->namespace, "string",
+                                     dsk_zlang_stack_node_clear_string,
+                                     dsk_zlang_stack_node_copy_string);
+  dsk_zlang_namespace_register_type (&rv->namespace, "float",
+                                     dsk_zlang_stack_node_clear_do_nothing,
+                                     dsk_zlang_stack_node_copy_memcpy);
+  return rv;
+}
+
+dsk_boolean      dsk_zlang_context_parse_file (DskZLangContext *context,
+                                               const char      *filename,
+                                               DskZLangInsn   **main_out,
+                                               DskError       **error)
+{
+  ...
+}
+
+dsk_boolean      dsk_zlang_context_register   (DskZLangContext *context,
+                                               const char      *name,
+                                               DskZLangFunction *function,
+                                               DskError       **error);
+
+DskZLangStack   *dsk_zlang_stack_new          (DskZLangContext *context,
+                                               DskZLangHandler  handler,
+                                               DskZLangHandler  exception,
+                                               void            *handler_data,
+                                               DskDestroyNotify destroy);
+void             dsk_zlang_stack_run          (DskZLangStack   *stack,
+                                               DskZLangInsn    *insn);
+
+
+/* one of these functions must be invoked by DskZLangHandler */
+void dsk_zlang_handler_return (DskZLangStack     *stack);
+void dsk_zlang_handler_throw  (DskZLangStack     *stack,
+                               DskZLangStackNode *exception);
---
+++ dsk-zlang.h
@@ -0,0 +1,126 @@
+
+typedef enum
+{
+  DSK_ZLANG_TYPE_INT,
+  DSK_ZLANG_TYPE_UINT,
+  DSK_ZLANG_TYPE_UNICODE,
+  DSK_ZLANG_TYPE_STRING,
+  DSK_ZLANG_TYPE_FLOAT,
+  DSK_ZLANG_TYPE_DOUBLE,
+  DSK_ZLANG_TYPE_CODE_POINTER,
+  DSK_ZLANG_TYPE_CATCH,
+  DSK_ZLANG_INSN_FUNCTION
+} DskZLangType;
+
+typedef void         (*DskZLangHandler) (DskZLangStack     *stack,
+                                         void              *handler_data);
+typedef DskZLangInsn (*DskZLangCaseFunc)(DskZLangStackNode *test_val,
+                                         void              *case_data);
+  
+struct _DskZLangStackNode
+{
+  DskZLangType type;
+  union {
+    int v_int;
+    unsigned v_uint;
+    float v_float;
+    double v_double;
+    struct {
+      DskZLangInsn *activation;
+      unsigned n_params;
+      DskZLangStackNode *params;
+    } v_code_pointer;
+    struct {
+      DskZLangHandler handler;
+      void *handler_data;
+      DskDestroyNotify destroy;
+    } v_catch;
+    DskZLangFunction *v_function;
+    char *v_string;
+  };
+};
+struct _DskZLangStack
+{
+  unsigned stack_size;
+  unsigned stack_alloced;
+  DskZLangStackNode *stack;
+};
+  
+typedef enum
+{
+  DSK_ZLANG_INSN_PUSH,
+  DSK_ZLANG_INSN_DUP,
+  DSK_ZLANG_INSN_POP,
+  DSK_ZLANG_INSN_CALL,
+  DSK_ZLANG_INSN_CASE,
+  DSK_ZLANG_INSN_JUMP
+} DskZLangInsnType;
+
+struct _DskZLangInsn
+{
+  DskZLangInsnType type;
+  union {
+    struct { DskZLangStackNode *value; } v_push;
+    struct { unsigned source, dest; } v_dup;
+    unsigned v_pop;
+    struct {
+      DskZLangHandler handler;
+      void *handler_data;
+      unsigned n_args;
+      DskDestroyNotify destroy;
+    } v_call;
+    struct {
+      DskZLangCaseFunc case_func;
+      void *case_data;
+      DskDestroyNotify destroy;
+    } v_case;
+    DskZLangInsn *v_jump;
+  } u;
+};
+
+typedef struct _DskZLangFunction DskZLangFunction;
+struct _DskZLangFunction
+{
+  int n_args;                   /* -1 for any arity */
+  DskZLangHandler handler;
+  void *handler_data;
+  DskDestroyNotify destroy;
+  unsigned ref_count;
+};
+DskZLangFunction *dsk_zlang_function_new_c (int n_args,
+                                            DskZLangHandler handler,
+                                            void *handler_data,
+                                            DskDestroyNotify destroy);
+DskZLangFunction *dsk_zlang_function_new   (int           n_args,
+                                            unsigned      n_insn,
+                                            DskZLangInsn *insn);
+
+
+void dsk_zlang_stack_push     (DskZLangStack     *stack,
+                               DskZLangStackNode *node);
+void dsk_zlang_stack_pop      (DskZLangStack     *stack,
+                               unsigned           count);
+
+DskZLangContext *dsk_zlang_context_new        (void);
+dsk_boolean      dsk_zlang_context_parse_file (DskZLangContext *context,
+                                               const char      *filename,
+                                               DskZLangInsn   **main_out,
+                                               DskError       **error);
+dsk_boolean      dsk_zlang_context_register   (DskZLangContext *context,
+                                               const char      *name,
+                                               DskZLangFunction *function,
+                                               DskError       **error);
+
+DskZLangStack   *dsk_zlang_stack_new          (DskZLangContext *context,
+                                               DskZLangHandler  handler,
+                                               DskZLangHandler  exception,
+                                               void            *handler_data,
+                                               DskDestroyNotify destroy);
+void             dsk_zlang_stack_run          (DskZLangStack   *stack,
+                                               DskZLangInsn    *insn);
+
+
+/* one of these functions must be invoked by DskZLangHandler */
+void dsk_zlang_handler_return (DskZLangStack     *stack);
+void dsk_zlang_handler_throw  (DskZLangStack     *stack,
+                               DskZLangStackNode *exception);
