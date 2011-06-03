ccee2ea0f4d682c15f9d0a9490e32017d26aee17
Dave Benson <daveb@ffem.org>
2011-06-03 08:35:50 -0700

    social user protobuf-c

--- examples/social-network-daemon.c
+++ examples/social-network-daemon.c
@@ -1,6 +1,17 @@
 #include "../dsk-table.h"
 #include "../dsk-http-server.h"
 
+/* FORMATS ---
+  Generally:
+    - 40-bit ints are used for user-ids
+
+  Specific formats:
+    - User -  (the value for both the by-id and by-name tables)
+      - 1-byte version number (0 for now)
+      - 40-bit id
+      - NUL-terminated name
+    - User -  (the value for both the by-id and by-name tables)
+
 struct _User
 {
   uint64_t id;
@@ -20,33 +31,115 @@ struct _Message
 static DskTable *name_to_user;
 static DskTable *userid_to_user;
 static DskTable *word_userid_to_messageid;
-static uint64_t  next_user_id;          /* mmapped */
+static uint64_t *next_user_id;          /* mmapped */
+
+/* --- utility functions --- */
+static User *
+parse_user (unsigned len, const uint8_t *data)
+{
+  ...
+}
+static User *
+lookup_user_by_id (uint64_t userid)
+{
+  ...
+}
+static User *
+lookup_user_by_name (const char *name)
+{
+  ...
+}
+
+/* NOTE: takes ownership of 'user' (for possible caching) */
+static void
+write_user (User *user)
+{
+  /* serialize */
+  ...
+
+  /* add to "by-id" table */
+  ...
+
+  /* add to "by-name" table */
+  ...
+}
+
+
+/* --- CGI handlers --- */
+
+/* cgi helpers */
+
+/* 'mandatory' means we respond with an error
+   if the CGI variable isn't available. */
+static const char *
+get_cgi_var (DskHttpServerRequest *request,
+             const char           *variable_name,
+             dsk_boolean           mandatory)
+{
+  for (i = 0; i < request->n_cgi_variables; i++)
+    if (strcmp (request->cgi_variables[i].name, variable_name) == 0)
+      return request->cgi_variables[i].value;
+  if (mandatory)
+    {
+      unsigned len = strlen (variable_name) + 100;
+      char *msg = dsk_malloc (len);
+      snprintf (msg, len, "no CGI variable %s found", variable_name);
+      dsk_http_server_request_respond_error (request,
+                                             DSK_HTTP_STATUS_BAD_REQUEST, msg);
+      dsk_free (msg);
+    }
+  return NULL;
+}
 
 static void
 cgi_handler__add_user (DskHttpServerRequest *request,
                        void                 *func_data)
 {
+  const char *name = get_cgi_var (request, "name", DSK_TRUE);
+  if (name == NULL)
+    return;
+
   /* lookup username: fail if user already exists */
-  ...
+  User *user = lookup_user_by_name (name);
+  if (user != NULL)
+    {
+      ...
+    }
 
   /* XXX: should be atomic: (allocate user-id, and the two inserts) */
 
+  user = dsk_malloc (sizeof (User) + strlen (name) + 1);
+  user->name = (char *) (user + 1);
+  strcpy (user->name, name);
+  user->n_friends = 0;
+  user->friends = NULL;
+
+
   /* allocate user-id */
-  ...
-
-  /* insert entries into name_to_user */
-  ...
-
-  /* insert entries into userid_to_user */
-  ...
+  user->user_id = *next_user_id;
+  *next_user_id += 1;
 
   /* respond with user-id */
   DskHttpServerResponseOptions options = DSK_HTTP_SERVER_RESPONSE_OPTIONS_DEFAULT;
   options.content_type = "text/plain";
-  snprintf (buf, sizeof (buf), "%llu", user_id);
+  snprintf (buf, sizeof (buf), "%llu", user->id);
   options.content_body = buf;
   options.content_length = strlen (buf);
   dsk_http_server_respond (request, &options);
+
+  write_user (user);            /* takes ownership of user */
+}
+
+static void
+cgi_handler__lookup_user (DskHttpServerRequest *request,
+                          void                 *func_data)
+{
+  /* 
+  /* lookup username: fail if user already exists */
+  const char *name = get_cgi_var (request, "name", DSK_TRUE);
+  if (name == NULL)
+    return;
+  ...
 }
 
 static void
@@ -139,6 +232,13 @@ cgi_handler__blather        (DskHttpServerRequest *request,
   ...
 
   /* respond with message id */
+  ...
+}
+
+static void
+cgi_handler__search    (DskHttpServerRequest *request,
+                        void                 *func_data)
+{
   ...
 }
 
