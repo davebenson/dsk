def35f1b58873eefdb2d5bcebc2d184c5e3f94f4
Dave Benson <daveb@ffem.org>
2011-09-13 07:42:58 -0700

    ...

--- dsk-strv.h
+++ dsk-strv.h
@@ -2,6 +2,9 @@
 
 
 unsigned  dsk_strv_length  (char **strs);
+void      dsk_strv_lengths (char **strs,
+                            unsigned *n_strings_out,            /* optional */
+                            unsigned *total_string_bytes_out);  /* optional */
 char    **dsk_strv_concat  (char **a_strs, char **b_strs);
 char    **dsk_strvv_concat (char ***str_arrays);
 char    **dsk_strv_copy    (char **strs);
