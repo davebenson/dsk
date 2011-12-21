8555d6c114b251d4da2187049286d29b5eefb10f
Dave Benson <daveb@ffem.org>
2011-12-20 18:45:29 -0800

    comment

--- dsk-daemon.h
+++ dsk-daemon.h
@@ -1,7 +1,12 @@
 
-/* --dsk-log-template=STRFTIME_FORMAT
- * --dsk-pidfile=FILENAME
- * --dsk-daemonize
+/* --dsk-fork: fork and run in the background
+   --dsk-watchdog: restart if the program dies
+   --dsk-pid-filename FILENAME: file to write PID to, lock
+   --dsk-log-template STRFTIME_FILENAME: rotating file for output
+   --dsk-log-timezone TZ: use timezone when computing logfile names
+   --dsk-alert-script SCRIPT: run sh script if subprocess dies
+   --dsk-alert-interval SECONDS: do not run alert-script more than once 
+                                 in this number of seconds.
  */
 
 
