2616e89fa397595cbea62c45f2bc40c8a51187e9
Dave Benson <daveb@ffem.org>
2011-12-20 18:45:53 -0800

    work on jmux

---
+++ programs/jmux/jmux-nb-lang.h
@@ -0,0 +1,6 @@
+
+/* A script is parsed as a function w/ no arguments. */
+JmuxNblangFunction *jmux_nblang_parse              (...);
+JmuxNblangFunction *jmux_nblang_lookup_subfunction (JmuxNblangFunction *,
+                                                    const char *name);
+
