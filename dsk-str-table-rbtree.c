124b2c7574181cc73b7118de849d786434cafb3d
Dave Benson <daveb@juvomedia.com>
2012-01-04 15:05:04 -0800

    str-table

---
+++ dsk-str-table-rbtree.c
@@ -0,0 +1,271 @@
+#include "dsk-str-table.h"
+
+typedef struct _DskStrTableRbtree DskStrTableRbtree;
+struct _DskStrTableRbtree
+{
+  DskStrtableRbtreeNode *top;
+  size_t value_size;
+  void *default_value;
+};
+typedef struct _DskStrTableRbtreeNode DskStrTableRbtreeNode;
+struct _DskStrTableRbtreeNode
+{
+  DskStrtableRbtreeNode *parent, *left, *right;
+  dsk_boolean is_red;
+  /* value follows, then string follows that */
+};
+
+#define GET_NODE_STRING(node) \
+  ((char*)(node) + sizeof (DskStrtableRbtreeNode) + rbtree->value_size)
+
+#define COMPARE_RBTREE_NODES(a,b, rv) \
+  rv = strcmp (GET_NODE_STRING (a), GET_NODE_STRING (b))
+#define COMPARE_STR_TO_RBTREE_NODE(a,b, rv) \
+  rv = strcmp (a, GET_NODE_STRING (b))
+
+#define GET_RBTREE()   \
+  rbtree->top, DskStrtableRbtreeNode *, DSK_STD_GET_IS_RED, \
+  DSK_STD_SET_IS_RED, parent, left, right, \
+  COMPARE_RBTREE_NODES
+
+static void 
+str_table_rbtree__init(size_t         value_size,
+                       const void    *value_default,
+                       DskStrTable   *to_init)
+{
+  DskStrTableRbtree *rbtree = (DskStrTableRbtree *) to_init;
+  rbtree->top = NULL;
+  rbtree->value_size = value_size;
+  if (value_default)
+    rbtree->default_value = dsk_memdup (value_size, value_default);
+  else
+    rbtree->default_value = NULL;
+}
+
+static void *
+str_table_rbtree__get (DskStrTable   *table,
+                       const char    *str)
+{
+  DskStrTableRbtree *rbtree = (DskStrTableRbtree *) to_init;
+  DskStrtableRbtreeNode *node;
+  DSK_RBTREE_LOOKUP_COMPARATOR (GET_RBTREE (), str, COMPARE_STR_TO_RBTREE_NODE,
+                                node);
+  if (node == NULL)
+    return NULL;
+  else
+    return node + 1;
+}
+
+static void *
+str_table_rbtree__get_prev     (DskStrTable   *table,
+                     const char    *str,
+                     const char   **key_out)
+{
+  DSK_RBTREE_INFIMUM_COMPARATOR (GET_RBTREE (), str, COMPARE_STR_TO_RBTREE_NODE,
+                                 node);
+  if (node == NULL)
+    return NULL;
+  if (strcmp (GET_NODE_STRING (node), str) == 0)
+    DSK_RBTREE_PREV (GET_RBTREE (), node, node);
+  if (node == NULL)
+    return NULL;
+  if (key_out != NULL)
+    key_out = GET_NODE_STRING (node);
+  return node + 1;
+}
+
+static void *
+str_table_rbtree__get_next     (DskStrTable   *table,
+                     const char    *str,
+                     const char   **key_out)
+{
+  DSK_RBTREE_SUPREMUM_COMPARATOR (GET_RBTREE (),
+                                  str, COMPARE_STR_TO_RBTREE_NODE,
+                                  node);
+  if (node == NULL)
+    return NULL;
+  if (strcmp (GET_NODE_STRING (node), str) == 0)
+    DSK_RBTREE_NEXT (GET_RBTREE (), node, node);
+  if (node == NULL)
+    return NULL;
+  if (key_out != NULL)
+    key_out = GET_NODE_STRING (node);
+  return node + 1;
+}
+
+static void *
+str_table_rbtree__force        (DskStrTable   *table,
+                                const char    *str)
+{
+  DskStrTableRbtree *rbtree = (DskStrTableRbtree *) table;
+  DskStrtableRbtreeNode *node;
+  DSK_RBTREE_LOOKUP_COMPARATOR (GET_RBTREE (),
+                                str, COMPARE_STR_TO_RBTREE_NODE,
+                                node);
+  if (node != NULL)
+    return node + 1;
+  len = strlen (str);
+  node = dsk_malloc (sizeof (DskStrtableRbtreeNode) + rbtree->value_size
+                     + len + 1);
+  memcpy (GET_NODE_STRING (node), str, len + 1);
+  DSK_RBTREE_INSERT (GET_RBTREE (), node, conflict);
+  dsk_assert (conflict == NULL);
+  if (rbtree->default_value)
+    memcpy (node + 1, rbtree->default_value, rbtree->value_size);
+  else
+    memset (node + 1, 0, rbtree->value_size);
+  return node + 1;
+}
+
+static void *
+str_table_rbtree__force_noinit (DskStrTable   *table,
+                                const char    *str,
+                                dsk_boolean *created_opt_out)
+{
+  DskStrTableRbtree *rbtree = (DskStrTableRbtree *) table;
+  DskStrtableRbtreeNode *node;
+  DSK_RBTREE_LOOKUP_COMPARATOR (GET_RBTREE (),
+                                str, COMPARE_STR_TO_RBTREE_NODE,
+                                node);
+  if (node != NULL)
+    {
+      if (created_opt_out)
+        *created_opt_out = DSK_FALSE;
+      return node + 1;
+    }
+  len = strlen (str);
+  node = dsk_malloc (sizeof (DskStrtableRbtreeNode) + rbtree->value_size
+                     + len + 1);
+  memcpy (GET_NODE_STRING (node), str, len + 1);
+  DSK_RBTREE_INSERT (GET_RBTREE (), node, conflict);
+  dsk_assert (conflict == NULL);
+  if (created_opt_out)
+    *created_opt_out = DSK_TRUE;
+  return node + 1;
+}
+
+static void *
+str_table_rbtree__create       (DskStrTable   *table,
+                                const char    *str)
+{
+  DskStrTableRbtree *rbtree = (DskStrTableRbtree *) table;
+  DskStrtableRbtreeNode *node;
+  unsigned len;
+  DskStrtableRbtreeNode *conflict;
+  DSK_RBTREE_LOOKUP_COMPARATOR (GET_RBTREE (),
+                                str, COMPARE_STR_TO_RBTREE_NODE,
+                                node);
+  if (node != NULL)
+    return NULL;
+  len = strlen (str);
+  node = dsk_malloc (sizeof (DskStrtableRbtreeNode) + rbtree->value_size
+                     + len + 1);
+  memcpy (GET_NODE_STRING (node), str, len + 1);
+  DSK_RBTREE_INSERT (GET_RBTREE (), node, conflict);
+  dsk_assert (conflict == NULL);
+  if (rbtree->default_value)
+    memcpy (node + 1, rbtree->default_value, rbtree->value_size);
+  else
+    memset (node + 1, 0, rbtree->value_size);
+  return node + 1;
+}
+
+static dsk_boolean  
+str_table_rbtree__remove(DskStrTable   *table,
+                         const char    *str,
+                         void          *value_opt_out)
+{
+  DskStrtableRbtreeNode *node;
+  DSK_RBTREE_LOOKUP_COMPARATOR (GET_RBTREE (),
+                                str, COMPARE_STR_TO_RBTREE_NODE,
+                                node);
+  if (node == NULL)
+    return DSK_FALSE;
+  if (value_opt_out)
+    memcpy (value_opt_out, node + 1, rbtree->value_size);
+  DSK_RBTREE_REMOVE (GET_RBTREE (), node);
+  dsk_free (node);
+  return DSK_TRUE;
+}
+
+static void *
+str_table_rbtree__create_or_remove_noinit
+                    (DskStrTable   *str_table,
+                     const char    *str)
+{
+  ...
+  DskStrtableRbtreeNode *node;
+  DSK_RBTREE_LOOKUP_COMPARATOR (GET_RBTREE (),
+                                str, COMPARE_STR_TO_RBTREE_NODE,
+                                node);
+  if (node)
+    {
+      /* remove */
+      DSK_RBTREE_REMOVE (GET_RBTREE (), node);
+      return NULL;
+    }
+  else
+    {
+      /* create */
+      unsigned len = strlen (str);
+      DskStrtableRbtreeNode *node = dsk_malloc (sizeof (DskStrtableRbtreeNode) + rbtree->value_size
+                         + len + 1);
+      DskStrtableRbtreeNode *conflict;
+      memcpy (GET_NODE_STRING (node), str, len + 1);
+      DSK_RBTREE_INSERT (GET_RBTREE (), node, conflict);
+      dsk_assert (conflict == NULL);
+      return node + 1;
+    }
+}
+
+static void *
+str_table_rbtree__create_or_remove
+                    (DskStrTable   *table,
+                     const char    *str)
+{
+  void *rv = str_table_rbtree__create_or_remove_noinit (table, str);
+  if (rv != NULL)
+    {
+      if (rbtree->default_value)
+        memcpy (rv, rbtree->default_value, rbtree->value_size);
+      else
+        memset (rv, 0, rbtree->value_size);
+    }
+  return rv;
+}
+
+static void  
+str_table_rbtree__set(DskStrTable   *str_table,
+                      const char    *str,
+                      const void    *value)
+{
+  DskStrTableRbtree *rbtree = (DskStrTableRbtree *) table;
+  void *nv = str_table_rbtree__force_noinit (str_table, str, NULL);
+  memcpy (nv, value, rbtree->value_size);
+}
+
+static dsk_boolean 
+str_table_rbtree__foreach(DskStrTable   *str_table,
+                          const char    *prefix,
+                          DskStrTableForeachFind foreach,
+                          void          *foreach_data)
+{
+  ...
+}
+
+DskStrTableInterface dsk_str_table_interface_rbtree =
+{
+  sizeof (DskStrTableRbtree),
+  str_table_rbtree__init,
+  str_table_rbtree__get,
+  str_table_rbtree__get_prev,
+  str_table_rbtree__get_next,
+  str_table_rbtree__force,
+  str_table_rbtree__force_noinit,
+  str_table_rbtree__create,
+  str_table_rbtree__remove,
+  str_table_rbtree__create_or_remove,
+  str_table_rbtree__create_or_remove_noinit,
+  str_table_rbtree__set,
+  str_table_rbtree__foreach
+};
--- dsk-str-table.h
+++ dsk-str-table.h
@@ -1,4 +1,6 @@
 
+
+
 DskStrTable *dsk_str_table_new (size_t value_size,
                                 const void *value_default);
 
@@ -9,54 +11,92 @@ void        *dsk_str_table_get     (DskStrTable *,
                                     const char *str);
 void        *dsk_str_table_get_prev(DskStrTable *,
                                     const char *str,
-				    char       *key_out);
+				    const char **key_out);
 void        *dsk_str_table_get_next(DskStrTable *,
                                     const char *str,
-				    char       *key_out);
+				    const char **key_out);
+void        *dsk_str_table_force   (DskStrTable *,
+                                    const char *str);
+void        *dsk_str_table_force_noinit 
+                                   (DskStrTable *,
+                                    const char *str,
+                                    dsk_boolean *created_opt_out);
+void        *dsk_str_table_create  (DskStrTable *,
+                                    const char *str);
+dsk_boolean  dsk_str_table_delete  (DskStrTable *,
+                                    const char *str,
+                                    void       *value_opt_out);
+void        *dsk_str_table_create_or_delete
+                                   (DskStrTable *,
+                                    const char *str);
+void        *dsk_str_table_create_or_delete_noinit
+                                   (DskStrTable *,
+                                    const char *str);
 void         dsk_str_table_set     (DskStrTable *,
                                     const char *str,
                                     const void *value);
 
-/* Only sets if the entry is not found.
-   Returns TRUE if the 'set'-operation worked
-   (meaning that the key was created by this operation) */
-dsk_boolean  dsk_str_table_maybe_set(DskStrTable *str_table,
-                                     const char  *str,
-                                     const void  *value);
+typedef void (*DskStrTableForeach) (const char *str,
+                                    void       *value,
+                                    void       *foreach_data);
 
-void       dsk_str_table_foreach   (DskStrTable *...,
+void       dsk_str_table_foreach   (DskStrTable *str_table,
                                     const char *prefix,
-                                    ...);
+                                    DskStrTableForeach foreach,
+                                    void       *foreach_data);
 
+typedef dsk_boolean (*DskStrTableForeachFind) (const char *str,
+                                               void       *value,
+                                               void       *foreach_data);
 /* dsk_str_table_foreach_find is an arguably weak name choice.
    it is intended to mimick a linear-searching "find" function
    which returns TRUE as soon as a value is found that matches
    the criteria implemented in 'func'.  (And returns FALSE otherwise) */
-dsk_boolean dsk_str_table_foreach_find (DskStrTable *...,
+dsk_boolean dsk_str_table_foreach_find (DskStrTable *str_table,
                                         const char *prefix,
-                                        ...);
+                                        DskStrTableForeachFind func,
+                                        void       *foreach_data);
 
-/* --- general interface - handles queries and sets --- */
-typedef enum
+struct _DskStrTable
 {
-/* can be passed if you want to initialize the slot yourself. */
-  DSK_STR_TABLE_SLOT_NO_INIT          = (1<<0),
+  DskStrTableInterface *iface;
+};
 
-  /* Exactly one if these options should be given! */
-#define _DSK_STR_TABLE_SLOT_MODE(n) ((n)<<8)
-  DSK_STR_TABLE_SLOT_CREATE           = _DSK_STR_TABLE_SLOT_MODE(0),
-  DSK_STR_TABLE_SLOT_MUST_CREATE      = _DSK_STR_TABLE_SLOT_MODE(1),
-  DSK_STR_TABLE_SLOT_DELETE_IF_EXISTS = _DSK_STR_TABLE_SLOT_MODE(2),
-  DSK_STR_TABLE_SLOT_DELETE_OR_CREATE = _DSK_STR_TABLE_SLOT_MODE(3),
-  DSK_STR_TABLE_SLOT_MUST_EXIST       = _DSK_STR_TABLE_SLOT_MODE(4),
-  DSK_STR_TABLE_SLOT_PREDECESSOR      = _DSK_STR_TABLE_SLOT_MODE(5),
-  DSK_STR_TABLE_SLOT_SUCCESSOR        = _DSK_STR_TABLE_SLOT_MODE(6),
-} DskStrTableSlotFlags;
-
-#define _DSK_STR_TABLE_SLOT_MODE_MASK    _DSK_STR_TABLE_SLOT_MODE_MASK(0xf)
-
-void        *dsk_str_table_slot(DskStrTable         *table,
-                                DskStrTableSlotFlags flags,
-                                const char         **key_opt_out,
-				dsk_boolean         *created_opt_out,
-				const char          *str);
+struct _DskStrTableInterface
+{
+  size_t sizeof_table;
+  void (*init)          (size_t         value_size,
+                         const void    *value_default,
+                         DskStrTable   *to_init);
+  void *(*get)          (DskStrTable   *table,
+                         const char    *str);
+  void *(*get_prev)     (DskStrTable   *table,
+                         const char    *str,
+                         const char   **key_out);
+  void *(*get_next)     (DskStrTable   *table,
+                         const char    *str,
+                         const char   **key_out);
+  void *(*force)        (DskStrTable   *table,
+                         const char    *str);
+  void *(*force_noinit) (DskStrTable   *table,
+                         const char    *str,
+                         dsk_boolean *created_opt_out);
+  void *(*create)       (DskStrTable   *table,
+                         const char    *str);
+  dsk_boolean  (*remove)(DskStrTable   *table,
+                         const char    *str,
+                         void          *value_opt_out);
+  void *(*create_or_remove)
+                        (DskStrTable   *table,
+                         const char    *str);
+  void *(*create_or_remove_noinit)
+                        (DskStrTable   *str_table,
+                         const char    *str);
+  void  (*set)          (DskStrTable   *str_table,
+                         const char    *str,
+                         const void    *value);
+  dsk_boolean (*foreach)(DskStrTable   *str_table,
+                         const char    *prefix,
+                         DskStrTableForeachFind foreach,
+                         void          *foreach_data);
+};
