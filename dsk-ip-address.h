aad39ae9a1ddb672d6d03c7e8e84c6c1928bffc4
Dave Benson <daveb@ffem.org>
2017-08-17 17:30:48 -0700

    spacing

--- dsk-ip-address.h
+++ dsk-ip-address.h
@@ -13,26 +13,26 @@ struct _DskIpAddress
 };
 #define DSK_IP_ADDRESS_INIT {0,{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}}
   
-dsk_boolean dsk_hostname_looks_numeric (const char *str);
-dsk_boolean dsk_ip_address_parse_numeric (const char *str,
-                                           DskIpAddress *out);
-char *dsk_ip_address_to_string (const DskIpAddress *);
+dsk_boolean    dsk_hostname_looks_numeric   (const char         *str);
+dsk_boolean    dsk_ip_address_parse_numeric (const char         *str,
+                                             DskIpAddress       *out);
+char          *dsk_ip_address_to_string     (const DskIpAddress *address);
 
-dsk_boolean dsk_ip_addresses_equal (const DskIpAddress *a,
-                                    const DskIpAddress *b);
+dsk_boolean    dsk_ip_addresses_equal       (const DskIpAddress *a,
+                                             const DskIpAddress *b);
 
 
 /* --- interfacing with system-level sockaddr structures --- */
 /* 'out' should be a pointer to a 'struct sockaddr_storage'.
  */
-void dsk_ip_address_to_sockaddr (const DskIpAddress *address,
-                                  unsigned       port,
-                                  void          *out,
-                                  unsigned      *out_len);
-dsk_boolean dsk_sockaddr_to_ip_address (unsigned addr_len,
-                                         const void *addr,
-                                         DskIpAddress *out,
-                                         unsigned      *port_out);
+void        dsk_ip_address_to_sockaddr (const DskIpAddress *address,
+                                        unsigned            port,
+                                        void               *out,
+                                        unsigned           *out_len);
+dsk_boolean dsk_sockaddr_to_ip_address (unsigned            addr_len,
+                                        const void         *addr,
+                                        DskIpAddress       *out,
+                                        unsigned           *port_out);
 
 void dsk_ip_address_localhost (DskIpAddress *out);
 
@@ -41,10 +41,10 @@ void dsk_ip_address_localhost (DskIpAddress *out);
       getpeername  -- returns the other end's address.
    returns FALSE for non-ip sockets, etc.
  */
-dsk_boolean dsk_getsockname (DskFileDescriptor fd,
-                             DskIpAddress     *addr_out,
-                             unsigned         *port_out);
-dsk_boolean dsk_getpeername (DskFileDescriptor fd,
-                             DskIpAddress     *addr_out,
-                             unsigned         *port_out);
+dsk_boolean dsk_getsockname            (DskFileDescriptor   fd,
+                                        DskIpAddress       *addr_out,
+                                        unsigned           *port_out);
+dsk_boolean dsk_getpeername            (DskFileDescriptor   fd,
+                                        DskIpAddress       *addr_out,
+                                        unsigned           *port_out);
 
