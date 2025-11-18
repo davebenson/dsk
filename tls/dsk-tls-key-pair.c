a2d863b5df56bfd9fd58b05ff9e2518a5ec6a8ad
Dave Benson <dave.benson@embodied.me>
2025-11-17 20:43:59 -0800

    dsk cleanups

--- tls/dsk-tls-key-pair.c
+++ tls/dsk-tls-key-pair.c
@@ -15,3 +15,37 @@ DskTlsKeyPairClass dsk_tls_key_pair_class =
   NULL,   // sign
   NULL,   // verify
 };
+
+
+bool   dsk_tls_key_pair_supports_scheme (DskTlsKeyPair     *kp,
+                                         DskTlsSignatureScheme algorithm)
+{
+  return DSK_TLS_KEY_PAIR_GET_CLASS (kp)->supports_scheme (kp, algorithm);
+}
+
+//size_t dsk_tls_key_pair_get_hash_length (DskTlsKeyPair     *kp,
+//                                         DskTlsSignatureScheme scheme)
+//{
+//  return DSK_TLS_KEY_PAIR_GET_CLASS (kp)->get_hash_length (kp, scheme);
+//}
+//
+size_t dsk_tls_key_pair_get_signature_length
+                                        (DskTlsKeyPair     *kp,
+                                         DskTlsSignatureScheme scheme)
+{
+  return DSK_TLS_KEY_PAIR_GET_CLASS (kp)->get_signature_length (kp, scheme);
+}
+bool   dsk_tls_key_pair_has_private_key (DskTlsKeyPair     *kp)
+{
+  return DSK_TLS_KEY_PAIR_GET_CLASS (kp)->has_private_key (kp);
+}
+void   dsk_tls_key_pair_sign            (DskTlsKeyPair     *kp,
+                                         DskTlsSignatureScheme scheme,
+                                         size_t             content_len,
+                                         const uint8_t     *content_data,
+                                         uint8_t           *signature_out);
+void   dsk_tls_key_pair_verify          (DskTlsKeyPair     *kp,
+                                         DskTlsSignatureScheme scheme,
+                                         size_t             content_len,
+                                         const uint8_t     *content_data,
+                                         const uint8_t     *sig);
